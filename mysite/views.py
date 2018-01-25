311f508c0246d371b0bb24041c857e25a1d6c496
nonop1507 <nono1507@naver.com>
2018-01-25 20:53:15 +0900

    git ignore

--- mysite/views.py
+++ mysite/views.py
@@ -8,6 +8,20 @@ from .models import UserData
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        form = UserDataForm(request.POST)
+        qs = UserData.objects.all()
+        id_check = request.POST.get("user_id", "")
+        pwd_check = request.POST.get("user_pwd", "")
+        print(id_check, '+', pwd_check)
+        if qs.filter(user_id=id_check, user_pwd=pwd_check):
+            print('complete.html')
+            return redirect('complete')
+        else:
+            print('fail')
+            return redirect('index')
+    else:
+        form = UserDataForm
     return render(request, 'login/index.html', {})
 
 
@@ -16,7 +30,7 @@ def register(request):
         form = UserDataForm(request.POST)
         qs = UserData.objects.all()
         body = request.POST.get("user_id", "")
-        if qs.filter(user_id__contains=body):
+        if qs.filter(user_id=body):
             return redirect('register')
         if form.is_valid():
             user_data = form.save(commit=False)
@@ -27,14 +41,6 @@ def register(request):
         form = UserDataForm
     return render(request, 'login/register.html', {'form': form})
 
-# if request.method == "POST":
-#        form = PostForm(request.POST)
-#        if form.is_valid():
-#            post = form.save(commit=False)
-#            post.author = request.user
-#            post.published_date = timezone.now()
-#            post.save()
-#            return redirect('post_detail', pk=post.pk)
-#    else:
-#        form = PostForm()
-#    return render(request, 'blog/post_edit.html', {'form': form})
+
+def complete(request):
+    return render(request, 'login/complete.html')
